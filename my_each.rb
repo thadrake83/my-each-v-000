eb8f3e7428b4752aa685e78d8e6dbe895b5f18ee
Drake Rentrop <drakerentrop@gmail.com>
2018-11-12 21:12:37 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,29 @@
-def my_each # put argument(s) here
-  # code here
+list = ["ab", "cd", "ef"]
+
+def my_each(array)
+  i = 0
+  while i < array.length
+# The "while" method will always return a value of => nil"
+    yield(array[i])
+    i = i + 1
+  end
+  array
 end
+
+my_each(list) {|i|  "So I think #{i} is an integer"}
+
+# Use the collection method
+# To make the *return-value* the answer you want
+# instead of "=> nil"
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+hello(list) {|i| "Hello #{i} what's up?"}
